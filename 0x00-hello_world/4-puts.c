99e0c6d85e617429c7aa130b5a44ae6ee432a5e1
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-09-08 23:54:43 -0700

    C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+
 /* this code prints something to the scream */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
-	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
