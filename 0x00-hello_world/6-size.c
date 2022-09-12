f1d5d1212603cb0157b4a342cb3374178efe07d2
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-09-11 18:15:37 -0700

    prints the size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 * This program  prints the size of various types on the computer it is compiled and run on.
 * Return: 0
 */
-imt main(void)
+int main(void)
 {
 	char c;
 	int i;
@@ -16,8 +16,8 @@ imt main(void)
 
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	retune (0);
 }
