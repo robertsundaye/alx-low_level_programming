491b2c0773e9ef8adb1df2cf2c79bcdd3c008dea
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-09-19 18:53:16 -0700

    good

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code
+ * @n: pointerto n to be updated
+ * Description: print using the main function
+ * This code swap the variables' valuas
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+}
+	*n = 98;
+}
