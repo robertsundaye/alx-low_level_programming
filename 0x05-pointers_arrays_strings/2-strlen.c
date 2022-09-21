0c9be0b437cc4cc58e2238ae86249720922578be
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-09-21 16:50:24 -0700

    task_2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * 
+ * @s: inpute string
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index} != '\0' ; index++)
+
+	return (index);
+}
