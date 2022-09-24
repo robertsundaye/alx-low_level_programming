356cce428cc297fa6acc05caa1ca88a7aeb49029
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-09-24 06:37:47 -0700

    task-4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+int r;
+int g;
+s = "ROBERT"
+g = strlen(s);
+for (r = g-1 ; r >= 0 ; r--)
+{
+	_putchar(s[r]);
+}
+}
