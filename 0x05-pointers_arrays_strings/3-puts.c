9ec2c38aa878aa9649fa1fbe8008e7c16b3a2faa
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-09-24 05:43:04 -0700

    task-3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * _puts - prints a string follow by a new new*
@@ -9,7 +9,7 @@ void _puts(char *str)
 int index;
 index = 0;
 
-while (str[index] != '\0;)
+while (str[index] != '\0')
 {
 	_putchar(str[index]);
 }
