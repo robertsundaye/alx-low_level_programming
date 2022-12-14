6a7f94d107d8ae3f8be0c817e7d5c83eb5db294a
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-14 23:48:43 +0000

    pd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - checks the code in the function
+ * @r: takes the value from the function
+ * Return: returns absolute value
+ */
+int _abs(int  r)
+{
+	if (r >=)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
