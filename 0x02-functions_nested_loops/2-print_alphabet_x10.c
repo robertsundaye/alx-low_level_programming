2e3a5f2e40b9461a55cb85ecd336ad398fbc71f0
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-14 14:04:44 +0000

    pd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,13 +12,13 @@ void print_alphabet_x10(void)
 	int t;
 	char r;
 
-	for (t = 1; t <= 10;t++)
+	for (t = 1; t <= 10; t++)
 
 	{
 		for (r = 'a'; r <= 'z'; r++)
 
 		{
-			_putchar(r); 
+			_putchar(r);
 		}
 		_putchar('\n');
 	}
