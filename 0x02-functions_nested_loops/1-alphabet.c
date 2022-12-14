af43526791920139d692ed8d02a774002144d1c9
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-14 02:30:43 +0000

    printing a_z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int chr = 'a';
+	while(chr <= 'z')
+	{
+		putchar(chr);
+		chr++;
+	}
+	putchar('\n');
+	return (0);
+}
