5f21689a85966953a394edb428b25c79c42aec90
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-14 15:31:42 +0000

    pd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - search for lower case letter
  * @c:  takes tge import
- * Return - returns 1 if the statement is true or 0 if not
+ * Return: returns 1 if the statement is true or 0 if not
  */
 
 int _islower(int c)
