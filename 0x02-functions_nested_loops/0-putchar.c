ffb1a92a7c72f4426a70d77c2db268313a8e8ffa
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-14 01:55:48 +0000

    using putchar function to print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	
+
+	return (0);
+}
