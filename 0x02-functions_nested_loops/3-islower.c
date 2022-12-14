8d413923c277bde6157d0692f0ab4d5f7ab93a5a
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-14 15:07:15 +0000

    searching for lower case letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * -islower - search for lower case letter
+ */
+
+int _islower(int c)
+{
+	if (c => 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_alphabet_10(void);
+int main()
+{
+	print_alphabet_x10();
+	return (0);
+}
