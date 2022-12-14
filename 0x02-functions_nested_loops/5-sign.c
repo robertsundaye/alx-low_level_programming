fa49f94a3ea32d7755e0d2dfa839c6c1c3663c2e
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-14 22:52:44 +0000

    pd

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the code
+ * @n: take the value from the function
+ * Return: 1 if true, 0 if true for the second statement, -1 if false
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n = 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+		return (-1);
+}
