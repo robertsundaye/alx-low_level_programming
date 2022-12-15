7df6f8fedcc4ee092c144d7773514b43beb85e49
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-15 01:35:19 +0000

    pd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			{
 				for (c = 0; c <= 5; c++)
 				{
-					for (d = 0; d <= 5; d++)
+					for (d = 0; d <= 9; d++)
 					{
 						_putchar(a + '0');
 						_putchar(b + 'a');
