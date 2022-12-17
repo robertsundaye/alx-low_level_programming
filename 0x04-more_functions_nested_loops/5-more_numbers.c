c3876970870c78cd6937c3622af8fc643f2fd3f5
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-17 16:18:15 +0000

    pd

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,13 @@ void more_numbers(void)
 	int r;
 	int b;
 
-	for (r = '1'; r <= '10'; r++)
+	for (r = 1; r <= 10; r++)
 	{
-		for (b = '0'; b <= '14'; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
-			{
 				_putchar('1');
-				_putchar(b % 10 + '0');
-			}
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
