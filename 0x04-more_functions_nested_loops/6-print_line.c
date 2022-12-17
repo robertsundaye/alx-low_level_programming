b7e0851574b7e5f18fa2249a880ea000644a98b2
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-17 16:58:25 +0000

    pd

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,17 @@
 
 /**
  * print_line - Draws a straight line
+ *
  * @n: take an int as import
+ * Return: no result
  */
 void print_line(int n)
 {
 	int r;
 
 	for (r = 1; r <= n; r++)
-
 	{
-			_putchar(95);
+		_putchar(95);
 	}
 	_putchar('\n');
 }
