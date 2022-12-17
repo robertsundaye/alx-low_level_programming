f3803c10046af840f42a5d722bd46d90e375d208
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-17 14:57:11 +0000

    printing from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks the code
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int r;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar(r);
+	}
+	_putchar('\n');
+}
