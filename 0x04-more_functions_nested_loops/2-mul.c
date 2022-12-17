b8b44ae0396599e5b3cf075b19688e1b143a4ecf
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-17 14:46:45 +0000

    multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - checks the code in the function
+ * @a: takes value as input
+ * @b: takes value as input
+ * Return: returns c as @a and @b product
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	_putchar(c);
+
+	return (c);
+}
