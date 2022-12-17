37a5731cb756694cb453e38fae863a1a1e94bb4f
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-17 15:59:06 +0000

    pd

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,8 +14,10 @@ void more_numbers(void)
 		for (b = '0'; b <= '14'; b++)
 		{
 			if (b >= '10')
-				_putchar('r');
-			_putchar(b % 10 + '0');
+			{
+				_putchar('1');
+				_putchar(b % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
