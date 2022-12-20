63cda3acbc31196b9fed36f50fce70298faa36eb
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-20 02:52:29 +0000

    pd

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+
+	while (*str != '\0')
+	{
+		a++;
+		str++;
+	}
+	str -= (a / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
