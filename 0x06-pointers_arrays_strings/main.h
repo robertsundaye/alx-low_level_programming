7a6482d9b7a37cdf924b043b3f3c8f9260d6bd1e
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-23 18:25:24 +0000

    pd

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
