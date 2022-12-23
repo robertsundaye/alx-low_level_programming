4182053f6b8faf81d864b183979244419e09ed0d
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-23 14:36:11 +0000

    pd

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - joins two strings
+ * @dest: join to
+ * @src: join from
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	for (dest[a] = 0; dest[a] != '\0'; a++)
+	
+	int b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
