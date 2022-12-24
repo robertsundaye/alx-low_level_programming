e645eb885fe6f533f2e54dce5cfac74277605967
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-24 01:21:04 +0000

    pd

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,17 +3,13 @@
 /**
  * reverse_array - reverse content of an array
  * @a: array
- * @n: number of element
- * @r: input1
- * @c: input2
+ * @n: number of elemenit to be reverse
  * Return: rev
  */
 void reverse_array(int *a, int n)
 {
 	int r;
 	int c;
-
-
 	for (r = 0; r < n; r++)
 	{
 		n--;
