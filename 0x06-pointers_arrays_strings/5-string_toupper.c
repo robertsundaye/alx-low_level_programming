8cb742b88862192aa1e563f6dbeab9a9afadd848
Robert Sundaye <robertsundaye886314568@gmail.com>
2022-12-24 01:40:10 +0000

    pd

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lower case to upper case
+ * @a: pointer
+ * Return: char
+ */
+char *string_toupper(char *a)
+{
+	int b;
+
+	b = 0;
+	while (a[b] != '\0')
+	{
+		if (a[b] >= 'a' && a[b] <= 'z')
+		{
+			a[b] = a[b] - 32;
+		}
+		a++;
+	}
+	return (a);
+}
+
