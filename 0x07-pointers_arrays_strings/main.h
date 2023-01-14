10c3a03b3393b1e100bbcc2400f680328fb75aaa
Robert Sundaye <robertsundaye886314568@gmail.com>
2023-01-14 02:25:53 +0000

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,5 +9,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-
 #endif
