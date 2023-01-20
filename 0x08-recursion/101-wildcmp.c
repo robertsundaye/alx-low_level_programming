d63d065776e22fda678f33a65e53a19098e73485
Robert Sundaye <robertsundaye886314568@gmail.com>
2023-01-20 18:16:53 +0000

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -23,7 +23,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1, s2 + q) || wildcmp(s1 + 1, s2));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
