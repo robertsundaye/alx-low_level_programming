0e99576dffb8b818f521322d5e5fda5efc7cc625
Robert Sundaye <robertsundaye886314568@gmail.com>
2023-01-22 09:46:48 +0000

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,13 +30,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			s = "(nil)";
 		}
 		printf("%s", s);
-
 		if (i < n - 1)
 		{
 			printf("%s", separator);
 		}
 	}
 	printf("\n");
-
 	va_end(strings);
 }
