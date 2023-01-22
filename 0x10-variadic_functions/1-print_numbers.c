67dc11f2652c8cda040b083b1c4590b59f2ea1af
Robert Sundaye <robertsundaye886314568@gmail.com>
2023-01-22 09:23:47 +0000

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,8 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
-		
-		if (i < n - 1; && separator != NULL)
+		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
