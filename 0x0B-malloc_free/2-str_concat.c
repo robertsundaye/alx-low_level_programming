14bc47f52115042ef5dd9d6017636d02aceaadc2
Robert Sundaye <robertsundaye886314568@gmail.com>
2023-04-23 22:17:58 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,25 +12,39 @@ char *str_concat(char *s1, char *s2)
 	int r, s;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-
-	for (r = 0; s1[r] != '\0'; r++)
-	for (s = 0; s2[s] != '\0'; s++)
+	}
+	r = s = 0;
+	while (s1[r] != '\0')
+	{
+		r++;
+	}
+	while (s2[s] != '\0')
+	{
+		s++;
+	}
 	join = (char*)malloc(sizeof(char) * (r + s + 1));
 
 	if (join == NULL)
+	{
 		return (NULL);
-	for (r = 0; s1[r] != '\0'; r++)
+	}
+	r = s = 0;
+	while (s1[r] != '\0')
 	{
 		join[r] = s1[r];
+		r++;
 	}
 
-	for (s = 0; s2[s] != '\0'; s++)
+	while (s2[s] != '\0')
 	{
 		join[r] = s2[s];
-		r++;
+		r++, s++;
 	}
 	join[r] = '\0';
 	return (join);
