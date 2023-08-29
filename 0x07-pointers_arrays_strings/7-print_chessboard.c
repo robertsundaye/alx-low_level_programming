d03daa6cdb76adb3e46b66c842a58890c9b0ee80
Robert Sundaye <111018257+robertsundaye@users.noreply.github.com>
2023-08-30 00:17:27 +0100

    Create 7-print_chessboard.c
    C

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - print out the chessboard
+ * @(*a)[8]: 2D array
+ *
+ *Return: always Void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}
