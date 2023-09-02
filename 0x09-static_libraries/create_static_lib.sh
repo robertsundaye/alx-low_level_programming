47533b688f4760354c75cbbe3953f673bcbdcb5b
Robert Sundaye <111018257+robertsundaye@users.noreply.github.com>
2023-09-02 02:08:23 +0100

    Update create_static_lib.sh
    C

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# create an array containing all .c files in the current directory
-files=($(ls *.c))
-
-# compile all files
-for file in "${files[@]}"
-do
-	gcc -c -o "${file%.*}.o" "$file"
-done
-
-# create static library
-ar rcs liball.a *.o
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
