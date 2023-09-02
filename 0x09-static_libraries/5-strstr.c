9440c3c4ff7c157a926174657d3849e32baa4bee
Robert Sundaye <111018257+robertsundaye@users.noreply.github.com>
2023-09-02 02:19:37 +0000

    C

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,27 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * _strstr - locates a substring
- * @haystack: adress of string to be scanned
- * @needle: adress of refference string
- *
- *Return: always 0
+ * _strstr - Entry point
+ * @str1: input
+ * @str2: input
+ * Return: Always 0 (Success)
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; *haystack++)
-	{
-		char *i = haystack;
-		char *j = needle;
-		while (*i  == *j && *j != '\0')
-		{
-			i++;
-			j++;
-		}
-		if (*j == '\0')
-		{
-			return (haystack);
-		}
+    for (; *haystack != '\0'; haystack++)
+    {
+    	char *l = haystack;
+    	char *p = needle;
+ 
+    	while (*l == *p && *p != '\0')
+    	{
+        	l++;
+    		p++;
+    	}
+ 
+    	if (*p == '\0')
+        	return (haystack);
 	}
-	return (0);
+ 
+		return (0);
 }
