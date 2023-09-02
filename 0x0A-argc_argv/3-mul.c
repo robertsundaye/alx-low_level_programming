e9e6e580b5075299461f65b9847be82a131f709e
Robert Sundaye <111018257+robertsundaye@users.noreply.github.com>
2023-09-02 01:56:41 +0100

    Update 3-mul.c
    C

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 #include "main.h"
 
-int _atoi(const char *str);
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
 
 /**
  * main - multiplies two numbers
@@ -27,24 +71,4 @@ int main(int argc, char *argv[])
 	printf("%d\n", result);
 
 	return (0);
-}
-
-/**
- * _atoi - converts a string to an integer
- * @str: string to be converted
- *
- * Return: the int converted from the string
- */
-
-int _atoi(const char *str)
-{
-	int res = 0; // Initialize result 
-
-	// Iterate through all characters of input string and 
-	// update result 
-	for (int i = 0; str[i] != '\0'; ++i) 
-		res = res * 10 + str[i] - '0'; 
-
-	// return result. 
-	return (res); 
 }
