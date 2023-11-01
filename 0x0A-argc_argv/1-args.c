cdd127d9f30b914313584b9151b5d738a743703d
Robert Sundaye <111018257+robertsundaye@users.noreply.github.com>
2023-11-01 23:41:54 +0000

    Add files via upload

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - display the number of arguments passed to the program
+ * @argc: total number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
