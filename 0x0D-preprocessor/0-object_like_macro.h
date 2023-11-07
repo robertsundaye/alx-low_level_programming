bb1479b093a78f6a56fdcd95dcd4e19cf0f7511b
Robert Sundaye <111018257+robertsundaye@users.noreply.github.com>
2023-11-07 20:24:37 +0000

    Create 0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,18 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
+
+
+======================================
+
+1-pi.h CODE
+
+#ifndef PI_H
+#define PI_H
+
+#define PI 3.14159265359
+
+#endif
