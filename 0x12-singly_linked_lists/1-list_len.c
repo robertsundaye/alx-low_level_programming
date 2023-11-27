0f38c044c8aceab7e925f3d3e9f13a1e250445d3
Robert Sundaye <111018257+robertsundaye@users.noreply.github.com>
2023-11-27 04:51:03 +0000

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t a = 0;
+	size_t n = 0;
 
- while (h)
- {
- a++;
- h = h->next;
- }
- return (a);
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
